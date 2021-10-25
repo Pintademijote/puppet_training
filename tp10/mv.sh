77a6b9c570d41e3c7f125e8d2309462ab89b77a5
Pintademijote <pg.lemasle@gmail.com>
2021-10-25 14:40:59 +0200

    add tp10, create template

---
+++ tp10/mv.sh
@@ -0,0 +1,2 @@
+cp /home/vagrant/puppet/tp10/content.epp /etc/puppetlabs/code/environments/production/modules/nginx/templates/
+cp /home/vagrant/puppet/tp10/nginx_v6.pp /etc/puppetlabs/code/environments/production/modules/nginx/manifests/init.pp
