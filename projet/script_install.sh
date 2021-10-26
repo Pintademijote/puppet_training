9bac8be8624dd9b4936fc0488239e4b966a102d7
Pintademijote <pg.lemasle@gmail.com>
2021-10-26 14:47:32 +0200

    add yes to install docker

--- projet/script_install.sh
+++ projet/script_install.sh
@@ -2,6 +2,6 @@ firewall-cmd --zone=public --add-service=http
 firewall-cmd --zone=public --add-service=https
 yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-yum install docker-ce docker-ce-cli containerd.io
+yum install -y docker-ce docker-ce-cli containerd.io
 groupadd docker
 usermod -aG docker vagrant
