d700e2583250bbf963ed807664dbed4d3eed818d
Pintademijote <pg.lemasle@gmail.com>
2021-10-26 12:55:44 +0200

    fix path in run.sh for content.epp

--- tp12/run.sh
+++ tp12/run.sh
@@ -2,5 +2,5 @@ mkdir /etc/puppetlabs/code/environments/dev/
 mkdir /etc/puppetlabs/code/environments/dev/{manifests,modules}
 cp -rf /etc/puppetlabs/code/environments/production/modules/nginx/ /etc/puppetlabs/code/environments/dev/modules/nginx/
 cp -rf /etc/puppetlabs/code/environments/production/manifests/nginx_v7.pp /etc/puppetlabs/code/environments/dev/manifests/nginx_v7.pp
-cp -rf /home/vagrant/puppet/tp12/content.epp /etc/puppetlabs/code/environments/production/modules/nginx/templates/
+cp -rf /home/vagrant/puppet/tp12/content.epp /etc/puppetlabs/code/environments/dev/modules/nginx/templates/
 
