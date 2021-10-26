f4eed9f77b0dd8887edaf2c0c039e9b5bf70b170
Pintademijote <pg.lemasle@gmail.com>
2021-10-26 16:20:21 +0200

    add file to docker compose

--- projet/2_run_docker_compose.pp
+++ projet/2_run_docker_compose.pp
@@ -1,7 +1,17 @@
 include 'project'
 include 'docker'
 
+file { '/tmp/wordpress.yml':
+  ensure  => file,
+  source  => 'puppet:///modules/project/wordpress.yml',
+  path    => '/tmp/wordpress.yml',
+  owner => 'root',
+  group => 'root',
+  mode  => '0744',
+  notify  => docker_compose['docker_compose'],
+}
+
 docker_compose { 'docker_compose':
-  compose_files => ['/home/vagrant/puppet/projet/wordpress.yml'],
+  compose_files => ['/tmp/wordpress.yml'],
   ensure  => present,
 }
