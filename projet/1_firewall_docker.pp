4a413203b91d73ff4e76507a94da15e201ae8bd0
Pintademijote <pg.lemasle@gmail.com>
2021-10-26 16:08:33 +0200

    add root

--- projet/1_firewall_docker.pp
+++ projet/1_firewall_docker.pp
@@ -3,9 +3,9 @@ class project(String $path_to_script = '/home/vagrant/puppet/projet/') {
   file { '/tmp/script_install.sh':
     ensure  => file,
     source  => 'puppet:///project/files/script_install.sh',
-#    path    => '/tmp/script_install.sh',
-#    owner => 'root',
-#    group => 'root',
+    path    => '/tmp/script_install.sh',
+    owner => 'root',
+    group => 'root',
     notify  => Exec['bash /tmp/script_install.sh'],
   }
 
