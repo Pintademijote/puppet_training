1adb569dfbcf595361d54bc146afbe42970bd884
Pintademijote <pg.lemasle@gmail.com>
2021-10-26 16:01:49 +0200

    add copy to colde/modules

--- projet/mv.sh
+++ projet/mv.sh
@@ -8,3 +8,4 @@ cp /home/vagrant/puppet/projet/2_run_docker_compose.pp /etc/puppetlabs/code/envi
 cp -rf /home/vagrant/puppet/projet/wordpress.yml /etc/puppetlabs/code/environments/wordpress/modules/project/files/
 cp -rf /home/vagrant/puppet/projet/script_install.sh /etc/puppetlabs/code/environments/wordpress/modules/project/files/
 cp -rf /etc/puppetlabs/code/environments/wordpress/modules/project/ /etc/puppetlabs/code/environments/production/modules/project/
+cp -rf /etc/puppetlabs/code/environments/wordpress/modules/project/ /etc/puppetlabs/code/modules/project/
