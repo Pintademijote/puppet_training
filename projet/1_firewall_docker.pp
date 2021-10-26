0353bea1ea49d38692cfcddecca889e6d70d13ea
Pintademijote <pg.lemasle@gmail.com>
2021-10-26 16:17:02 +0200

    add mode to file

--- projet/1_firewall_docker.pp
+++ projet/1_firewall_docker.pp
@@ -6,6 +6,7 @@ class project(String $path_to_script = '/home/vagrant/puppet/projet/') {
     path    => '/tmp/script_install.sh',
     owner => 'root',
     group => 'root',
+    mode  => '0744'
     notify  => Exec['bash /tmp/script_install.sh'],
   }
 
